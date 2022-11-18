208146ea638b277c02de200470d6b8de6cceb227
StephanieCueva <stefy.9430@gmail.com>
2022-11-18 13:06:02 -0500

    Proyecto Final U2

--- js/cantidad.js
+++ js/cantidad.js
@@ -1,24 +1,5 @@
-        //initialising a variable name data
-        var data = 1;
-    
-        //printing default value of data that is 0 in h2 tag
-        document.getElementById("counting").innerText = data;
-    
-        //creation of increment function
-        function increment() {
-            data = data + 1;
-            document.getElementById("counting").innerText = data;
-        }
-        //creation of decrement function
-        function decrement() {
-          if(data>1){  
-          data = data - 1;
-          document.getElementById("counting").innerText = data; 
-          
-        }
-        }
+function changeImage(image) {
+  var img = document.getElementById("main_product_image");
+  img.src = image;
+}
 
-        function changeImage(image) {
-          var img = document.getElementById("main_product_image");
-          img.src = image;
-        }
